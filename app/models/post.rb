b63e6ffe60feb8d4da8dd7f126d89d3527ad8182
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-01-18 11:31:57 +0100

    Add relationship and query method to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  has_many :comments
+  has_many :likes
+  belongs_to :author, class_name: 'User'
+
+  after_save :update_post_counter
+
+  def update_post_counter
+    author.increment!(:post_counter)
+  end
+
+  def five_recent_comments
+    comments.order(created_at: :desc).last(5)
+  end
 end
