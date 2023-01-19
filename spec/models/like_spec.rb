99a9532b998004d6c67d82a2924829899752d1b2
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-01-19 09:19:42 +0100

    Add like methods test

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  user = User.create(
+    name: 'Doe Jogn',
+    photo: 'http://photo.com',
+    post_counter: 0
+  )
+
+  post = Post.create(
+    title: 'Title',
+    text: 'Text for the post',
+    comments_counter: 0,
+    likes_counter: 0,
+    author_id: user.id
+  )
+
+  like = Like.create(post: post, author: user)
+
+  context 'update_likes_counter' do
+    like.update_likes_counter
+
+    it ' increment likes_counter' do
+      expect(Post.find(post.id).likes_counter).eql?(post.likes_counter + 1)
+    end
+  end
+end
