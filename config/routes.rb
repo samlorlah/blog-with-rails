669ba0bf8029b68ff39f6ab694da2f9be623d777
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-01-28 09:23:49 +0100

    Update code review recommendations

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] do
-      resources :comments, only: [:create]
-      resources :likes, only: [:create]
+      resources :comments, only: [:create, :new]
+      resources :likes, only: [:create, :new]
     end
   end
   resources :posts, only: [:new, :create]
