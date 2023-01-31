744823d714b84e75e8fe7099cebe0ee35cff9956
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-01-31 21:16:37 +0100

    Install Bullet, Catch and Fix N+1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.includes(comments: [:author]).find(params[:id])
+    @user = User.find(params[:user_id])
+    @post = Post.includes([:author]).find(params[:id])
   end
 
   def new
