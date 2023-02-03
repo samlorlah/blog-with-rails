2c02912532c69063ebd29d69b1f7f5115c9366c3
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-02-03 19:07:36 +0100

    Add JTI to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_03_092208) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_03_171840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,8 +61,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_03_092208) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "role"
+    t.string "jti"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
