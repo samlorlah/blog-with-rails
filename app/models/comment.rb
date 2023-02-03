79d3721ac5652f351e46d9e45a14902c7f7e389e
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-02-03 12:10:53 +0100

    Update comment counter after delete

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,8 @@ class Comment < ApplicationRecord
   def update_comments_counter
     post.increment!(:comments_counter)
   end
+
+  def update_comment_counter_when_comment_deleted
+    post.decrement!(:comments_counter)
+  end
 end
