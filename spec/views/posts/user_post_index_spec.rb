c8fd6e481c9ffb1b964995a20f19ba821717134c
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-02-08 12:37:39 +0100

    Update spec/vies/posts/index

--- spec/views/posts/user_post_index_spec.rb
+++ spec/views/posts/user_post_index_spec.rb
@@ -2,8 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'user_posts/#index', type: :feature do
   before :each do
-    @user = User.first
-    visit user_posts_path(@user.id)
+    @user = User.create(name: 'John Doe',
+                          photo: 'https://dummypic.jpg',
+                          bio: 'Long text describing my bio')
+    @first_post = Post.create(author: @user, title: 'Post 1', text: 'This is my first post')
+    @second_post = Post.create(author: @user, title: 'post 2',
+                               text: 'This is my second post')
+
+    @comment1 = Comment.create(author: @user, post: @second_post, text: 'Comment 1')
+    @comment2 = Comment.create(author: @user, post: @second_post, text: 'Comment 2')
+    @comment3 = Comment.create(author: @user, post: @second_post, text: 'Spend the credits wisely :|')
+
+    visit user_path(@user)
+
+    click_link 'See all posts'
   end
   describe 'Users Posts index view' do
     it 'should show user profile picture' do
@@ -28,6 +40,14 @@ RSpec.describe 'user_posts/#index', type: :feature do
     it 'should show how many comments a post has' do
       post = @user.posts.first
       expect(page).to have_content("Comments: #{post.comments_counter}")
+    end
+    it 'should show how many likes a post has' do
+      post = @user.posts.first
+      expect(page).to have_content("Likes: #{post.likes_counter}")
+    end
+    it 'redirects to post show page when a post is clicked' do
+      find("a[id='show-btn-#{@first_post.id}']").click
+      expect(current_path).to match user_post_path(:user_id => @user.id, :id => @first_post.id)
     end
     it 'should have button for pagination' do
       expect(page).to have_button('Pagination')
