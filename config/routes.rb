fb765b1db7dd80f765bb1bbcc31b1a029b9969e8
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-02-09 09:07:57 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, default: { format: :json }
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -12,14 +12,5 @@ Rails.application.routes.draw do
       resources :likes, only: [:create, :new]
     end
   end
-  resources :posts, only: [:new, :create, :destroy]
-  resources :comments, only: [:destroy]
-
-  namespace :api do
-    resources :users, only: [] do
-      resources :posts, only: [:index] do
-        resources :comments, only: [:index, :create]
-      end
-    end
-  end
+  resources :posts, only: [:new, :create]
 end
